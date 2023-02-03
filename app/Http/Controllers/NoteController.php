7503b66c1944721aef4a86ed56560009cf829914
vinknie <64844166+vinknie@users.noreply.github.com>
2023-02-03 17:15:25 +0100

    +1

--- app/Http/Controllers/NoteController.php
+++ app/Http/Controllers/NoteController.php
@@ -19,4 +19,40 @@ class NoteController extends Controller
 
         return view('admin.noteVerba', compact('category','getCategory','getverbatim'));
     }
+
+    public function updatepositif(Request $request, $id_verbatim)
+    {
+        $verbatim = Verbatim::findOrFail($id_verbatim);
+
+        $positif = $request->input('positif');
+        $value = $request->input('value');
+
+        $verbatim->update([$positif => $value]);
+
+        return redirect()->back();
+    }
+
+    public function updateneutre(Request $request, $id_verbatim)
+    {
+        $verbatim = Verbatim::findOrFail($id_verbatim);
+
+        $neutre = $request->input('neutre');
+        $value = $request->input('value');
+
+        $verbatim->update([$neutre => $value]);
+
+        return redirect()->back();
+    }
+
+    public function updatenegatif(Request $request, $id_verbatim)
+    {
+        $verbatim = Verbatim::findOrFail($id_verbatim);
+
+        $negatif = $request->input('negatif');
+        $value = $request->input('value');
+
+        $verbatim->update([$negatif => $value]);
+
+        return redirect()->back();
+    }
 }
