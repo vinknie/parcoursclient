91a163307ee64530df4fc889e96eafaa3bfe9a47
tenzin <dhontenzin@gmail.com>
2023-03-23 16:45:06 +0100

    soft delete users and minor fixes

--- public/js/app.js
+++ public/js/app.js
@@ -17,3 +17,22 @@
 const submenuBtn  = document.querySelector("a.dropdown-toggle");
 const pageSubmenu = document.getElementById("pageSubmenu");
 if(submenuBtn) submenuBtn.addEventListener("click", () => pageSubmenu.classList.toggle('collapse'));  
+
+
+
+// liste d'utilisateur
+const trashedBtn = document.querySelector('.trash-user-container h2.cursor-pointer');
+const trashedTable = document.querySelector('#trashed-usertable');
+const trashedEyeIcon = document.querySelector('.trash-user-container h2.cursor-pointer i');
+
+if(trashedBtn) {
+   trashedBtn.addEventListener('click', () => {
+      trashedTable.classList.toggle('hidden');
+      trashedEyeIcon.classList.toggle('fa-eye')
+   })
+}
+
+// success msg cross btn
+const success__crossBtn = document.querySelector('.success__cross-btn');
+const successMsg = document.querySelector('.success-msg');
+if(success__crossBtn) success__crossBtn.addEventListener('click', () => successMsg.style.display = 'none');
